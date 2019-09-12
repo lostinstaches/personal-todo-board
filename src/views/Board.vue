41ca026d510dd46bf31988a0d74233c0fa83cbe1
Igor Katselenbogen <orange-juice@Igors-MacBook-Pro.local>
2019-09-12 17:36:53 +0300

    Lesson 2 - Created columns and default view

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -1,6 +1,25 @@
 <template>
   <div class="board">
-    {{ board }}
+    <div class="flex flex-row items-start">
+      <div class="column" v-for="(column, index) in board.columns" :key="index">
+        <div class="flex items-center mb-2 font-bold">
+          {{ column.name }}
+        </div>
+        <div class="list-reset">
+          <div class="task" v-for="(task, index) in column.tasks" :key="index">
+            <span class="w-full flex-no-shrink font-bold">
+              {{ task.name }}
+            </span>
+            <p
+              v-if="task.description"
+              class="w-full flex-no-shrink mt-1 text-sm"
+            >
+              {{ task.description }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
