8569fbc2dfb7d86f446fd62018fa2ee4efb24191
Igor Katselenbogen <orange-juice@Igors-MacBook-Pro.local>
2019-09-13 20:53:42 +0300

    Lesson 8 - Extracting Components

---
+++ src/components/ColumnTask.vue
@@ -0,0 +1,97 @@
+<template>
+  <div
+    class="task"
+    draggable
+    @dragover.prevent
+    @dragenter.prevent
+    @click="goToTask(task)"
+    @dragstart="pickupTask($event, taskIndex, columnIndex)"
+    @drop.stop="moveTaskOrColumn($event, column.tasks, columnIndex, taskIndex)"
+  >
+    <span class="w-full flex-no-shrink font-bold">
+      {{ task.name }}
+    </span>
+    <p v-if="task.description" class="w-full flex-no-shrink mt-1 text-sm">
+      {{ task.description }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    task: {
+      type: Object,
+      required: true
+    },
+    column: {
+      type: Object,
+      required: true
+    },
+    taskIndex: {
+      type: Object,
+      required: true
+    },
+    columnIndex: {
+      type: Object,
+      required: true
+    },
+    board: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    goToTask(task) {
+      this.$router.push({ name: 'task', params: { id: task.id } })
+    },
+    moveTaskOrColumn(e, toTasks, toColumnIndex, toTaskIndex) {
+      const type = e.dataTransfer.getData('type')
+
+      if (type === 'task') {
+        this.moveTask(
+          e,
+          toTasks,
+          toTaskIndex !== undefined ? toTaskIndex : toTasks.length
+        )
+      } else {
+        this.moveColumn(e, toColumnIndex)
+      }
+    },
+    moveTask(e, toTasks, toTaskIndex) {
+      const fromColumnIndex = e.dataTransfer.getData('from-column-index')
+      const fromTasks = this.board.columns[fromColumnIndex].tasks
+
+      const fromTaskIndex = e.dataTransfer.getData('from-task-index')
+      this.$store.commit('MOVE_TASK', {
+        fromTasks,
+        toTasks,
+        fromTaskIndex,
+        toTaskIndex
+      })
+    },
+    moveColumn(e, toColumnIndex) {
+      const fromColumnIndex = e.dataTransfer.getData('from-column-index')
+
+      this.$store.commit('MOVE_COLUMN', {
+        fromColumnIndex,
+        toColumnIndex
+      })
+    },
+    pickupTask(e, taskIndex, fromColumnIndex) {
+      e.dataTransfer.effectAllowed = 'move'
+      e.dataTransfer.dropEffect = 'move'
+
+      e.dataTransfer.setData('from-task-index', taskIndex)
+      e.dataTransfer.setData('from-column-index', fromColumnIndex)
+      e.dataTransfer.setData('type', 'task')
+    }
+  }
+}
+</script>
+
+<style>
+.task {
+  @apply flex items-center flex-wrap shadow mb-2 py-2 px-2 rounded bg-white text-grey-darkest no-underline;
+}
+</style>
